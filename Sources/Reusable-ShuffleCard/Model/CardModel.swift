e99a011413623a1cef94618feeffb4705369a1ea
Leo Wirasanto Laia <leowirasanto@gmail.com>
2024-04-16 01:46:25 +0800

    improvement: create model public initializer

--- Sources/Reusable-ShuffleCard/Model/CardModel.swift
+++ Sources/Reusable-ShuffleCard/Model/CardModel.swift
@@ -48,6 +48,44 @@ public struct Card: Codable {
     var expiredDate: String
     var action: CardAction?
     var cards: [Card]
+    
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(String.self, forKey: .id)
+        self.isParent = try container.decode(Bool.self, forKey: .isParent)
+        self.type = try container.decode(CardType.self, forKey: .type)
+        self.title = try container.decodeIfPresent(String.self, forKey: .title)
+        self.description = try container.decodeIfPresent(String.self, forKey: .description)
+        self.imageUrl = try container.decode(String.self, forKey: .imageUrl)
+        self.createdDate = try container.decode(String.self, forKey: .createdDate)
+        self.expiredDate = try container.decode(String.self, forKey: .expiredDate)
+        self.action = try container.decodeIfPresent(CardAction.self, forKey: .action)
+        self.cards = try container.decode([Card].self, forKey: .cards)
+    }
+    
+    public init(
+        id: String,
+        isParent: Bool,
+        type: CardType,
+        title: String?,
+        description: String?,
+        imageUrl: String,
+        createdDate: String,
+        expiredDate: String,
+        action: CardAction?,
+        cards: [Card]
+    ) {
+        self.id = id
+        self.isParent = isParent
+        self.type = type
+        self.title = title
+        self.description = description
+        self.imageUrl = imageUrl
+        self.createdDate = createdDate
+        self.expiredDate = expiredDate
+        self.action = action
+        self.cards = cards
+    }
 }
 
 public struct CardAction: Codable {
