a0c91f95f5d15baee9851769413db8946817d1a4
Leo Wirasanto Laia <leowirasanto@gmail.com>
2024-04-20 01:21:50 +0800

    fix: - update function and attr modifier to public

--- Sources/Reusable-ShuffleCard/Core/ReusableShuffleCard.swift
+++ Sources/Reusable-ShuffleCard/Core/ReusableShuffleCard.swift
@@ -11,14 +11,14 @@ import SwiftUI
 @MainActor
 public class ReusableShuffleCard: ObservableObject {
     private var cardModel: CardModel = CardModel()
-    @Published var cardView: any View
+    @Published public var cardView: any View
     
     public init(_ style: CardStyle) {
         let view = ShuffleCardViewWrapper(style).environmentObject(cardModel)
         self.cardView = view
     }
     
-    func set(_ cardData: Card) {
+    public func set(_ cardData: Card) {
         DispatchQueue.main.async {
             self.cardModel.set(cardData)
         }
